92b3ab4fb97a4704aa349e5ca2c3146ed83d9987
Serhii Frolov <dunkan.aidaho@gmail.com>
2025-02-16 18:57:32 +0100

    Initial Next.js boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # natalie-homepage
+
+Build containers. Add -up flag to bring services up after build.
+
+```sh
+$> docker compose build
+```
+
+
+Bring containers up. Add -d flag to run output detached from current shell.
+
+```sh
+$> docker compose up
+```
+
+
+Bring containers down. Add -v flag to also delete named volumes
+
+```sh
+$> docker compose down
+```
+
+
+View logs by service name.
+
+```sh
+$> docker compose logs <service-name>
+```
+
+
+Enter shell for specified container (must be running)
+
+```sh
+$> docker exec -it <container-name> sh
+```
+
+
+Stop other containers in case port conflicts
+
+``` sh
+$> docker stop $(docker ps -a -q)
+```
---
+++ frontend/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  plugins: [
+    "@typescript-eslint/eslint-plugin",
+    "eslint-plugin-tsdoc",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "next/core-web-vitals",
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    },
+  },
+  rules: {
+    "tsdoc/syntax": "warn",
+    "react/no-unescaped-entities": 0,
+    "no-unexpected-multiline": "warn",
+    "semi": ["warn", "never", { "beforeStatementContinuationChars": "always" }],
+    "semi-style": ["warn", "first"],
+    "@typescript-eslint/no-unused-vars": [
+	  "error",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+	  }
+    ],
+    // "@typescript-eslint/no-misused-promises": [2, {
+    //   "checksVoidReturn": {
+    //     "attributes": false
+    //   }
+    // }],
+  },
+  ignorePatterns: [".next", "config/*", ".eslintrc.js", "vitest.config.js"],
+};
