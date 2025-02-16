4ae5070dd5b47a9417eaa1bafcd7cde423880a80
Serhii Frolov <dunkan.aidaho@gmail.com>
2025-02-16 21:48:56 +0100

    i18n placeholder

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en', 'pl', 'uk'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en',
+  },
 }
 
 export default nextConfig;
