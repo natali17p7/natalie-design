c67c9703c079ab1ae428c5a6c1c6bdd8d1970b16
Serhii Frolov <dunkan.aidaho@gmail.com>
2025-03-01 09:18:46 +0100

    Fix color theme

--- frontend/src/components/ExternalLink.tsx
+++ frontend/src/components/ExternalLink.tsx
@@ -12,10 +12,10 @@ export default function ExternalLink({description, href, title}: Props) {
       rel="noreferrer"
       target="_blank"
     >
-      <p className="text-xl font-semibold text-white">
+      <p className="text-xl font-semibold">
         {title} <span className="ml-2 inline-block">→</span>
       </p>
-      <p className="mt-2 max-w-[250px] text-gray-400">{description}</p>
+      <p className="mt-2 max-w-[250px]">{description}</p>
     </a>
   )
 }
--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -12,14 +12,11 @@ export default function PageLayout({children, title}: Props) {
 
   return (
     <div className="bg-slate-850 relative flex grow flex-col py-36">
-      <div className="absolute inset-0 overflow-hidden">
-        <div className="absolute left-0 top-1 size-[20500px] translate-x-[-47.5%] rounded-full bg-gradient-to-b from-slate-900 via-cyan-500" />
-      </div>
       <div className="container relative flex grow flex-col px-4">
-        <h1 className="text-3xl font-semibold leading-tight tracking-tight text-white md:text-5xl">
+        <h1 className="text-3xl font-semibold leading-tight tracking-tight md:text-5xl">
           {title}
         </h1>
-        <div className="mt-6 text-gray-400 md:text-lg">{children}</div>
+        <div className="mt-6 md:text-lg">{children}</div>
         <div className="mt-auto grid grid-cols-1 gap-4 pt-20 md:grid-cols-2 lg:gap-12">
           <ExternalLink
             description={t('links.docs.description')}
