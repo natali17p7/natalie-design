c76b4100624c360ffad69de9e6f885bf106b0c94
Serhii Frolov <dunkan.aidaho@gmail.com>
2025-03-11 21:17:03 +0100

    Fix code linting

--- eslint.sh
+++ eslint.sh
@@ -6,4 +6,4 @@ for arg in "$@"; do
     processed_arg="${arg/#frontend\//./}"
     args+=("$processed_arg")
 done
-pnpm eslint "${args[@]}" --fix
+pnpm tsc --noEmit && pnpm eslint "${args[@]}" --fix && pnm prettier src --write
