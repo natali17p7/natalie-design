5b2227be604f53ba387c0d32d6f0481cf887a568
Serhii Frolov <dunkan.aidaho@gmail.com>
2025-03-11 22:37:44 +0100

    Deal with unused vars in manifest

--- frontend/src/app/manifest.ts
+++ frontend/src/app/manifest.ts
@@ -2,13 +2,12 @@ import { MetadataRoute } from "next"
 import { getTranslations } from "next-intl/server"
 
 type Props = {
-  params: { locale: string }
+  _params: { locale: string }
 }
 
 export default async function manifest({
-  params,
+  _params,
 }: Props): Promise<MetadataRoute.Manifest> {
-  const { locale } = params
   const t = await getTranslations("Manifest")
 
   return {
