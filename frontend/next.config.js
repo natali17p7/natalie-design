fbe6c687d66f4baed0210703c243eac87c70419d
Serhii Frolov <dunkan.aidaho@gmail.com>
2025-03-12 11:45:18 +0100

    Add bundle analyzer

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,18 @@
-// @ts-check
+ // @ts-check
+ const withNextIntl = require('next-intl/plugin')();
+ const withBundleAnalyzer = require('@next/bundle-analyzer')({
+   enabled: process.env.ANALYZE === 'true',
+ });
 
-const withNextIntl = require('next-intl/plugin')();
+ /** @type {import('next').NextConfig} */
+ const config = {
+   webpack: config => {
+     config.resolve.fallback = {...config.resolve.fallback, fs: false};
+     return config;
+   },
+   // output: 'standalone'
+ };
 
-/** @type {import('next').NextConfig} */
-const config = {
-  webpack: config => {
-    config.resolve.fallback = {...config.resolve.fallback, fs: false}
-    return config
-  },
-};
-
-module.exports = withNextIntl(config);
+ module.exports = withBundleAnalyzer(
+   withNextIntl(config)
+ );
