85aeb232f733190416e82101a3cc2614ae77aea8
Serhii Frolov <dunkan.aidaho@gmail.com>
2025-03-14 08:54:14 +0100

    fix: Static generation for /projects

--- frontend/src/app/[locale]/projects/layout.tsx
+++ frontend/src/app/[locale]/projects/layout.tsx
@@ -2,13 +2,16 @@ import { routing } from "../../../i18n/routing"
 import { getProjects, type ProjectData } from "./utils"
 
 export function generateStaticParams() {
-  return routing.locales.flatMap(locale => {
-    const projects = getProjects(locale)
-    return projects.map((project: ProjectData) => ({
-      slug: project.slug,
-      locale: locale,
-    }))
-  })
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    return routing.locales.flatMap(locale => {
+      const projects = getProjects(locale)
+      return projects.map((project: ProjectData) => ({
+        slug: project.slug,
+        locale: locale,
+      }))
+    })
+  }
+  return []
 }
 
 export default function ProjectsLayout({
